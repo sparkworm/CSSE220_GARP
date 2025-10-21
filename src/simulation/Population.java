f8c45b84f3f97b2099722dcb3592a4d0b9ca2e03
David Clutter <clutteda@rose-hulman.edu>
2025-10-21 13:12:10 -0400

    Added basic body to Population

--- src/simulation/Population.java
+++ src/simulation/Population.java
@@ -1,4 +1,39 @@
 package simulation;
 
+import java.util.ArrayList;
+
+/**
+ * Class responsible for managing the set of Chromosomes being evolved.
+ */
 public class Population {
+    ArrayList<Chromosome> chromosomes;
+
+    public Population(int numChromosomes, int chromosomeSize) {
+        chromosomes = new ArrayList<Chromosome>(numChromosomes);
+        for (int i=0; i<numChromosomes; i++) {
+            chromosomes.add(new Chromosome(chromosomeSize, true));
+        }
+    }
+    public Population(ArrayList<Chromosome> chromosomes) {
+        this.chromosomes = chromosomes;
+    }
+
+    /**
+     * TODO: IMPLEMENT
+     * Applies a mutation to every Chromosome of the population.
+     * @param mutation the Mutation object that will be applied to every Chromosome.
+     */
+    public void mutateChromosomes(Mutation mutation) {
+        System.err.println("mutateChromosomes not yet implemented");
+    }
+
+    /**
+     * TODO: IMPLEMENT
+     * Finds average hamming distance between every Chromosome in the Population
+     * @return average hamming distance
+     */
+    public double calculateDiversity() {
+        System.err.println("calculateDiversity not yet implemented");
+        return Double.NaN;
+    }
 }
