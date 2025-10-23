7d731d216503b23cfc8682a917997ffc360a91d0
David Clutter <clutteda@rose-hulman.edu>
2025-10-22 22:05:35 -0400

    Added class for 2D vectors.

---
+++ src/utility/Vector2Int.java
@@ -0,0 +1,73 @@
+package utility;
+
+import java.util.Vector;
+
+public class Vector2Int {
+    private int x;
+
+    private int y;
+
+    public Vector2Int(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    public Vector2Int() {
+        this(0,0);
+    }
+
+    /**
+     * Finds the opposite of this vector, such that both elements are opposite sign of the original
+     * @return a new vector in the opposite direction of the original.
+     */
+    public Vector2Int opposite() {
+        return new Vector2Int(-x,-y);
+    }
+    
+    /**
+     * Returns the sum of the provided vectors
+     * @param vec1
+     * @param vec2
+     * @return sum of vec1 and 2
+     */
+    public static Vector2Int add(Vector2Int vec1, Vector2Int vec2) {
+        return new Vector2Int(vec1.getX() + vec1.getX(), vec2.getY() + vec2.getY());
+    }
+
+    /**
+     * Increases the value of the vector by other
+     * @param other the vector being added to this one
+     */
+    public void increaseBy(Vector2Int other) {
+        this.x += other.getX();
+        this.y += other.getY();
+    }
+
+    /**
+     * Returns the sum of the vertical and horizontal components of the vector.
+     * @return absolute value of x + absolute value of y
+     */
+    public int manhattanLength() {
+        return Math.abs(x) + Math.abs(y);
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setXY(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
