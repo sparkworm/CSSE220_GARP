08241efcb44f2b0863e77e1891ea8207805a262e
David Clutter <clutteda@rose-hulman.edu>
2025-11-03 12:01:26 -0500

    Basic implementation for EvolutionSimulator, as well as the skeleton of a test therefor.

--- src/simulation/EvolutionSimulator.java
+++ src/simulation/EvolutionSimulator.java
@@ -6,12 +6,23 @@ package simulation;
 public class EvolutionSimulator {
     private int generation;
     private boolean crossoverActive;
+    private double surviveRatio;
     private Population population;
+    private Fitness fitness;
     private Selection selection;
     private Crossover crossover;
     private Mutation mutation;
-    private Fitness fitness;
 
+    public EvolutionSimulator(int genotypeSize, int populationSize, double mutationRate, double surviveRatio) {
+        this.generation = 0;
+        this.crossoverActive = false; // change when crossover is more stable
+        this.surviveRatio = surviveRatio;
+        this.population = new Population(populationSize, genotypeSize); // create random population of specified size
+        this.fitness = new FitnessMaxOnes();  // TODO: replace with real fitness function
+        this.selection = new Selection(fitness);
+        this.crossover = new Crossover();  // TODO: implement real crossover for when crossoverActive==true
+        this.mutation = new Mutation(mutationRate);
+    }
 
     /**
      * Runs the simulation for the provided number of generations.  <br>
@@ -30,6 +41,15 @@ public class EvolutionSimulator {
      * Increments generation.
      */
     public void simulateGeneration() {
-        System.err.println("simulateGeneration not yet implemented");
+        generation++;
+        // Create new population from the fittest
+        population = new Population(crossover.repopulate(selection.trunctationSelection(population.getChromosomes(), surviveRatio), population.getSize()));
+        // Mutate new population
+        mutation.mutateChromosomes(population.getChromosomes());
+        System.out.println(String.format("Population, generation %d:  %s", generation, population.toString()));
+    }
+
+    public void setMutationRate(double mutationRate) {
+        mutation.setMutationRate(mutationRate);
     }
 }
