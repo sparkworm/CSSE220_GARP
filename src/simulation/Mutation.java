3891ffb74987ff30783b6f40948e94eaffaeb22a
David Clutter <clutteda@rose-hulman.edu>
2025-11-03 10:11:29 -0500

    Added starting logic to mutation to apply a very basic mutation to a list of chromosomes.  May override with more advanced functionality later.

--- src/simulation/Mutation.java
+++ src/simulation/Mutation.java
@@ -1,4 +1,31 @@
 package simulation;
 
+import java.util.ArrayList;
+
+/**
+ * Class responsible for mutating all the chromosomes in the population.  If other kinds of mutation (besides random
+ * flips) are wanted in the future, this class should be made abstract and the function mutateChromosomes should be
+ * overridden.
+ */
 public class Mutation {
+    private double mutationRate;
+
+    public Mutation(double mutationRate) {
+        this.mutationRate = mutationRate;
+    }
+
+    /**
+     * Mutates all Chromosomes in the provided list with the specified mutation method.  In this case, bits are simply
+     * randomly flipped, but this may be overridden in the future.
+     * @param chromosomes the list of chromosomes that will be modified
+     */
+    public void mutateChromosomes(ArrayList<Chromosome> chromosomes) {
+        for (Chromosome chrom : chromosomes) {
+            chrom.randomizeGenotype(mutationRate);
+        }
+    }
+
+    public void setMutationRate(double mutationRate) {
+        this.mutationRate = mutationRate;
+    }
 }
