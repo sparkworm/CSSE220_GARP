039103fdb48884eb7835284cebfb7359ddf5f5ab
David Clutter <clutteda@rose-hulman.edu>
2025-11-03 11:35:33 -0500

    Added Crossover and respective test.  At the moment, it doesn't actually 'cross over' but creates deep copies of the fittest Chromosomes.

--- src/simulation/Crossover.java
+++ src/simulation/Crossover.java
@@ -1,4 +1,25 @@
 package simulation;
 
+import java.util.ArrayList;
+
+/**
+ * Class responsible for 'breeding' surviving chromosomes from Selection such that the population is restored.
+ */
 public class Crossover {
+
+    /**
+     * Creates copies of the parents to repopulate
+     * TODO: Should be overridden with a true crossover function
+     * @param survivors the population from which the new population should be progenated
+     * @param targetPopulation the total number that the population should be rebuilt up to
+     */
+    public ArrayList<Chromosome> repopulate(ArrayList<Chromosome> survivors, int targetPopulation) {
+        int chromosomesToGo = targetPopulation - survivors.size();
+        // doesn't matter if references are shared since old population will soon be replaced.
+        ArrayList<Chromosome> newPopulation = new ArrayList<>(survivors);
+        for (int i=0; i<chromosomesToGo; i++) {
+            newPopulation.add(new Chromosome(survivors.get((i % survivors.size())))); // wraps back around
+        }
+        return newPopulation;
+    }
 }
---
+++ test/simulation/CrossoverTest.java
@@ -0,0 +1,46 @@
+package simulation;
+
+import java.util.ArrayList;
+
+import junit.framework.TestCase;
+
+public class CrossoverTest extends TestCase {
+
+    public void testCrossover() {
+        Selection selection = new Selection(new FitnessMaxOnes());
+        Crossover crossover = new Crossover();
+
+        ArrayList<Chromosome> chromArray = new ArrayList<>();
+        chromArray.add(new Chromosome("00101011"));
+        chromArray.add(new Chromosome("00101111"));
+
+        Population pop = new Population(chromArray);
+
+        System.out.println("Population:\n" + pop);
+
+        ArrayList<Chromosome> selected = selection.trunctationSelection(chromArray, 0.9);
+
+        Population selectedPop = new Population(selected);
+
+        System.out.println("Selected Pop:\n" + selectedPop);
+
+        Population newPop = new Population(crossover.repopulate(selectedPop.getChromosomes(), pop.getSize()));
+        System.out.println("New pop:\n" + newPop);
+
+        chromArray.add(new Chromosome("00111111"));
+        chromArray.add(new Chromosome("00001011"));
+        chromArray.add(new Chromosome("00101011"));
+        chromArray.add(new Chromosome("00101011"));
+
+
+        System.out.println("Population:\n" + pop);
+        selected = selection.trunctationSelection(chromArray, 0.2);
+        selectedPop = new Population(selected);
+
+        System.out.println("Selected Pop:\n" + selectedPop);
+
+        newPop = new Population(crossover.repopulate(selectedPop.getChromosomes(), pop.getSize()));
+        System.out.println("New pop:\n" + newPop);
+
+    }
+}
