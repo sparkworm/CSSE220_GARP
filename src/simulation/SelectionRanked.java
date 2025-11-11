a530fc032fd0da213ffc3eea2d314ef7e44b77a3
David Clutter <clutteda@rose-hulman.edu>
2025-11-11 10:02:23 -0500

    Added ranked selection.

---
+++ src/simulation/SelectionRanked.java
@@ -0,0 +1,103 @@
+package simulation;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+
+public class SelectionRanked extends Selection {
+    Random random;
+
+    SelectionRanked(Fitness fitness, Random random) {
+        super(fitness);
+        this.random = random;
+    }
+
+    @Override
+    public ArrayList<Chromosome> makeSelection(ArrayList<Chromosome> population, double surviveRatio) {
+        ArrayList<Chromosome> popCopy = new ArrayList<>(population);
+
+        int numSurvivors = calculateNumSurvivors(population.size(), surviveRatio);
+        ArrayList<Chromosome> survivors = new ArrayList<>(numSurvivors);
+        ArrayList<Double> fitnesses = new ArrayList<>(popCopy.size());
+        ArrayList<Double> sortedFitnesses = new ArrayList<>(popCopy.size());
+        for (Chromosome chrom : population) {
+            Double fit = fitness.calculateFitness(chrom);
+            fitnesses.add(fit);
+            sortedFitnesses.add(fit); // Double is immutable, so this shouldn't create problems
+        }
+
+        // With this array sorted, the index can correlate to the "rank":
+        // rank = length-idx;
+        // sumRank = (length/2)(length+1)
+        // position based on num [0, sumRank]:
+        Collections.sort(sortedFitnesses);
+
+        ArrayList<Double> rankedFitnesses = new ArrayList<>(sortedFitnesses.size());
+        for (int i=0; i<sortedFitnesses.size(); i++) {
+            rankedFitnesses.add((double)(i+1));
+        }
+
+        ArrayList<Double> unsortedRanks = new ArrayList<>(Collections.nCopies(sortedFitnesses.size(), null));
+        for (int i=sortedFitnesses.size()-1; i>=0; i--) {
+            for (int j=0; j<fitnesses.size(); j++) {
+                if (sortedFitnesses.get(i).equals(fitnesses.get(j)) && unsortedRanks.get(j)==null) {
+                    unsortedRanks.set(j, (double)(i+1));
+                    break; // Will end up assigning unique ranks to all, even if their fitness is identical.
+                }
+            }
+        }
+
+        double totalFitness = maxDepth(rankedFitnesses.size());
+
+        // Roulette Wheel selection (basically what's happening now that "ranks" are assigned as new fitness values)
+        for (int i=0; i<numSurvivors; i++) {
+            // Calculate a value between 0.0 and total fitness, this will determine how far the array of Chromosomes we
+            // make it before stopping, determining the chosen Chromosome
+            double depth = random.nextDouble() * totalFitness;
+
+            //int idx = findIdxFromDepth(depth, rankedFitnesses.size());
+
+            int idx = 0;
+            depth -= fitnesses.get(idx);
+            // Second condition ensures that the loop won't go too far even with floating point error
+            while(depth > 0 && idx < numSurvivors) {
+                idx++;
+                depth -= fitnesses.get(idx);
+            }
+            // idx is now the index of the chosen Chromosome
+            totalFitness -= rankedFitnesses.get(idx);
+            survivors.add(popCopy.get(idx));
+            rankedFitnesses.remove(idx);
+            popCopy.remove(idx);
+        }
+
+        return survivors;
+    }
+
+    /**
+     * CURRENTLY UNUSED
+     * Finds which Chromosome should be chosen based on a depth value from 0 to (length/2)*(length+1)
+     * NOTE: could probably solve better with DP and binary search
+     * @param depth works its way from the back of the array (which is the highest)
+     * @return
+     */
+    public int findIdxFromDepth(double depth, int arrayLength) {
+        int sum = 0;
+        for (int i=arrayLength-1; i>=0; i--) {
+            //sum += sortedFitnesses.get(i);  // might this work for roulette??
+            sum += i+1;
+            if (sum > depth) return i;
+        }
+        return 0;
+    }
+
+    /**
+     * Finds max depth based on the length of the array.  The max depth is simply the sum of all ranks in the array,
+     * or simply, a sum of all integers 1 through length, inclusive
+     * @param arrayLength
+     * @return
+     */
+    public int maxDepth(int arrayLength) {
+        return (int) (((double)arrayLength / 2.0) * (arrayLength + 1));  // Shouldn't see floating point error for 1/2
+    }
+}
--- test/simulation/SelectionTest.java
+++ test/simulation/SelectionTest.java
@@ -3,6 +3,7 @@ package simulation;
 import junit.framework.TestCase;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
 
 public class SelectionTest extends TestCase {
@@ -32,26 +33,80 @@ public class SelectionTest extends TestCase {
 
         Population pop = new Population(chroms1);
 
-        System.out.println("Population:\n" + pop);
+//        System.out.println("Population:\n" + pop);
 
         ArrayList<Chromosome> selected = selection.makeSelection(chroms1, 0.5);
 
         Population selectedPop = new Population(selected);
 
-        System.out.println("Selected Pop:\n" + selectedPop);
+//        System.out.println("Selected Pop:\n" + selectedPop);
 
 
-        System.out.println("Population:\n" + pop);
+//        System.out.println("Population:\n" + pop);
         selected = selection.makeSelection(chroms2, 0.5);
         selectedPop = new Population(selected);
 
-        System.out.println("Selected Pop:\n" + selectedPop);
+//        System.out.println("Truncation Selected Pop:\n" + selectedPop);
 
     }
 
-
+    // NOTE: only uses visual verification because assertion would be a pain here.  TODO?
     public void testSelectionRoulette() {
         Selection selection = new SelectionRoulette(new FitnessMaxOnes(), new Random());
+
+        Population pop = new Population(chroms1);
+
+//        System.out.println("Population:\n" + pop);
+
+        ArrayList<Chromosome> selected = selection.makeSelection(chroms1, 0.5);
+
+        Population selectedPop = new Population(selected);
+
+//        System.out.println("Selected Pop:\n" + selectedPop);
+
+//        System.out.println("Population:\n" + pop);
+        selected = selection.makeSelection(chroms2, 0.5);
+        selectedPop = new Population(selected);
+
+//        System.out.println("Roulette Selected Pop:\n" + selectedPop);
+    }
+
+    public void testFindIdxFromDepth() {
+        SelectionRanked selection = new SelectionRanked(new FitnessMaxOnes(), new Random());
+
+        // Values don't actually matter.  Would be equivalent to
+        // {1, 2, 3, 4, 5} in SelectionRoulette
+        // {15, 14, 12, 9, 5}
+        Double[] fitArray = {2.5, 3.0, 6.5, 7.0, 10.0};
+        ArrayList<Double> sortedFitnesses = new ArrayList<>(Arrays.asList(fitArray));
+
+        assertEquals(4, selection.findIdxFromDepth(4.4, sortedFitnesses.size()));
+        assertEquals(3, selection.findIdxFromDepth(5.4, sortedFitnesses.size()));
+        assertEquals(2, selection.findIdxFromDepth(10, sortedFitnesses.size()));
+        assertEquals(1, selection.findIdxFromDepth(13.9, sortedFitnesses.size()));
+        assertEquals(0, selection.findIdxFromDepth(15, sortedFitnesses.size()));
+    }
+
+    public void testMaxDepth() {
+        SelectionRanked selection = new SelectionRanked(new FitnessMaxOnes(), new Random());
+
+        assertEquals(1, selection.maxDepth(1));
+        assertEquals(3, selection.maxDepth(2));
+        assertEquals(6, selection.maxDepth(3));
+        assertEquals(10, selection.maxDepth(4));
+        assertEquals(15, selection.maxDepth(5));
+        assertEquals(21, selection.maxDepth(6));
+        assertEquals(28, selection.maxDepth(7));
+        assertEquals(36, selection.maxDepth(8));
+
+        assertEquals(3160, selection.maxDepth(79));
+
+        assertEquals(500500, selection.maxDepth(1000));
+    }
+
+    // NOTE: only uses visual verification because assertion would be a pain here.  TODO?
+    public void testSelectionRanked() {
+        Selection selection = new SelectionRanked(new FitnessMaxOnes(), new Random());
 
         Population pop = new Population(chroms1);
 
@@ -61,12 +116,10 @@ public class SelectionTest extends TestCase {
 
         Population selectedPop = new Population(selected);
 
-        System.out.println("Selected Pop:\n" + selectedPop);
+        System.out.println("Rank Selected Pop:\n" + selectedPop);
 
-        System.out.println("Population:\n" + pop);
         selected = selection.makeSelection(chroms2, 0.5);
         selectedPop = new Population(selected);
-
-        System.out.println("Selected Pop:\n" + selectedPop);
+        System.out.println("Rank Selected Pop:\n" + selectedPop);
     }
 }
